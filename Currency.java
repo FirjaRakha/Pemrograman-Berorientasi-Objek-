6281080283316abe5f00429cc4ae257d7d366e01
Firja Rakha <118196719+FirjaRakha@users.noreply.github.com>
2023-12-27 00:20:40 +0700

    Delete Currency.java

--- Currency.java
+++
@@ -1,6 +0,0 @@
-// Interface untuk definisikan pertukaran mata uang
-public interface Currency {
-    double getExchangeRate (String fromCurrency, String toCurrency);
-    double convertCurrency (double amount, String fromCurrency, String toCurrency);
-    String getCurrencyCode();
-}
